c53583a7650cda6d9b63f20af9a03d67b07646e2
unknown <jason.rettinger@gmail.com>
2022-10-13 09:58:10 -0400

    with help of askBCS found the temp error

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ const getLatAndLong = async (cityName) => {
 
 const getWeather = async (lat, lon) => {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`
   );
   const data = await response.json();
   return data;
